f45bce96cbd8571c5259bec0a63c1ff6b6fa247c
Jami <jami6bd@gmail.com>
2026-06-05 19:56:19 +0600

    new start

--- server/index.js
+++ server/index.js
@@ -96,4 +96,5 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`🌍 Environment: ${process.env.NODE_ENV || 'development'}`);
+  console.log('MONGO_URI=',process.env.MONGO_URI);
 });
